47c625e532a3c7e2c3ae81c8d513f09aaa4d3707
Carlos Sampol <sampol.90@gmail.com>
2019-07-01 11:46:03 -0500

    Adding some fixes to translation files

--- ui/app/pages/first-time-flow/end-of-flow/end-of-flow.component.js
+++ ui/app/pages/first-time-flow/end-of-flow/end-of-flow.component.js
@@ -60,15 +60,7 @@ export default class EndOfFlowScreen extends PureComponent {
           { '• ' + t('endOfFlowMessage7') }
         </div>
         <div className="first-time-flow__text-block end-of-flow__text-4">
-          *MetaMask cannot recover your seedphrase. <a
-            href="https://metamask.zendesk.com/hc/en-us/articles/360015489591-Basic-Safety-Tips"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <span className="first-time-flow__link-text">
-              Learn More
-            </span>
-          </a>.
+          *Callisto Hub cannot recover your seedphrase.
         </div>
         <Button
           type="primary"
